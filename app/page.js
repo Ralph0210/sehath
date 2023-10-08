c0d18ca0fb476242ce4e32a38cf73eb30295cba0
Ralph0210 <ralphchang@utexas.edu>
2023-10-08 16:30:16 -0500

    homepage hero image styled

--- app/page.js
+++ app/page.js
@@ -1,95 +1,57 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import styles from "./page.module.css";
+import Image from "next/image";
+import hero from "../public/new2.png";
+import corner from "../public/corner.png";
+import { motion } from "framer-motion";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
+  const fadeIn = {
+    hidden: { opacity: 1, y: 80 }, // Start with opacity 0 and slight Y translation
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } }, // Fade in and move up smoothly
+  };
+
+  return (
+    <>
+      <div className={styles.heroContainer}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src={hero}
+          className={styles.heroImage}
+          sizes="100vw"
+          alt="hero image"
           priority
         />
+        <div className={styles.heroText}>
+          <h1 className={styles.h1}>Bridges Health Worldwide</h1>
+          <p className={styles.p}>Empowering UT for Global Health Equity</p>
+        </div>
+        <motion.div className={styles.leftShape}
+        viewport={{ once: true, amount: 0.3 }}
+        variants={fadeIn}
+        initial="hidden"
+        whileInView="visible"
+        transition={{ ease: "easeOut", duration: 1 }}>
+          <Image
+            src={corner}
+            className={styles.cornerImage}
+            sizes="100vw"
+            alt="corner image"
+            priority
+          />
+          <div className={styles.bottomPart}>
+            <div className={styles.leftSquare}></div>
+            <Image
+              src={corner}
+              className={styles.cornerImage}
+              sizes="100vw"
+              alt="corner image"
+              priority
+            />
+          </div>
+        </motion.div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
